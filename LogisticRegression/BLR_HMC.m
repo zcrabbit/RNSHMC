7fbfd5f588f84a7c7f36636cf13df83c330b5455
chengzhang-uci <zc.rabbit@gmail.com>
2016-04-21 21:16:56 -0700

    no message

--- LogisticRegression/BLR_HMC.m
+++ LogisticRegression/BLR_HMC.m
@@ -4,26 +4,9 @@ load(['Data/' data]);
 NumOfIterations = 5000;
 BurnIn = floor(0.2*NumOfIterations);
 
-
-% Trajectory = 0.52;
-% NumOfLeapFrogSteps = 40;
-
-% parameters for 'lrdata'
-% Trajectory = 1.08;
-% NumOfLeapFrogSteps = 24;
-
-% Trajectory = 1.08;
-% NumOfLeapFrogSteps = 12;
-
-% Trajectory = 0.33;
-% NumOfLeapFrogSteps = 30;
-
-%parameter for adult data set
-%Trajectory = 0.52;
 NumOfLeapFrogSteps = nLeaps;
 
 alpha = 100;
-%StepSize = Trajectory/NumOfLeapFrogSteps;
 StepSize = stepsize;
 D = size(X,2);
 
@@ -32,40 +15,21 @@ Mass  =  diag(ones(D,1)*1);
 InvMass = sparse(inv(Mass));
 
 betaSaved = zeros(NumOfIterations-BurnIn,D);
-% Xtr = zeros(BurnIn-ColStart,D);
-% Ytr = zeros(BurnIn-ColStart,1);
 
 Startpoint = zeros(D,1);
-% Startpoint_adap = load('Startpoint_lrd28');
-% Startpoint_adap = Startpoint_adap.Startpoint;
-%Startpoint = [-2;zeros(D-1,1)];
-%Startpoint = [1;-1;zeros(D-2,1)];
 CurrentBeta = Startpoint;
 CurrentU = BLR_U(y,X,CurrentBeta,alpha);
-% Times = [0];
 
-% Random numbers
-% randn('state',2015);
-% rand('twister',2015);
 
-%%
 Proposed = 0;
 Accepted = 0;
-% ARate = [];
-% betaSam = [CurrentBeta'];
-% REM = [norm(CurrentBeta'-meanTrue)/norm(meanTrue)];
-% tic
+
 
 
 for IterationNum = 1:NumOfIterations   
     if mod(IterationNum,100) == 0
         disp([num2str(IterationNum) ' iterations completed.'])
         disp(Accepted/Proposed)
-%         if IterationNum > BurnIn
-%             ARate = [ARate Accepted/Proposed];
-%             %figure(1); plot((ColStart+100):100:IterationNum,ARate,'*-');
-%             %figure(1); semilogy(REM);
-%         end
 
         Proposed = 0;
         Accepted = 0;
@@ -108,19 +72,8 @@ for IterationNum = 1:NumOfIterations
         Accepted = Accepted + 1;
         
     end
-    
-%     betaSam = [betaSam; CurrentBeta'];
-%     REM = [REM norm(mean(betaSam,1)-meanTrue)/norm(meanTrue)];
-%     Times = [Times;toc];
-
-    % Start collection if required
-%     if IterationNum > ColStart && IterationNum <= BurnIn
-%         Xtr(IterationNum-ColStart,:) = CurrentBeta;
-%         Ytr(IterationNum-ColStart) = CurrentU;
-%     end
         
     % Save samples if required
-    %betaSaved(IterationNum,:) = CurrentBeta;
     if IterationNum > BurnIn
         betaSaved(IterationNum-BurnIn,:) = CurrentBeta;
     end
@@ -128,13 +81,6 @@ for IterationNum = 1:NumOfIterations
     % Start timer after burn-in
     if IterationNum == BurnIn
         disp('Burn-in complete, now drawing samples.')
-%         CurrentBeta = Startpoint_adap;
-%         CurrentU = BLR_U(y,X,CurrentBeta,alpha);
-%         betaSam = [CurrentBeta'];
-%         REM = [norm(CurrentBeta'-meanTrue)/norm(meanTrue)];
-%         base = min(Ytr); Ytr = Ytr - base;
-%         net = networkApprox([50,2000,1]);
-%         net = train(net,Xtr,Ytr,0.1);
         tic;
     end
 end
